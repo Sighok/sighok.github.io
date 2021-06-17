52d273898a4f671d9939249a5725b18d6ab8706a
Sighok <37277156+Sighok@users.noreply.github.com>
2021-06-17 12:56:31 +0200

    Add files via upload
    v0.1

--- script.js
+++ script.js
@@ -1,58 +1,57 @@
 
 function setHenviser(){
 	// Reset all inputs
-    document.querySelectorAll('input[type="checkbox"]').forEach(el => el.checked = false);
-    document.querySelectorAll('input[type="radio"]').forEach(el => el.checked = false);
-    document.querySelectorAll('textarea').forEach(el => el.value = "");
-    setDisplays()
+	document.querySelectorAll('input[type="checkbox"]').forEach(el => el.checked = false);
+	document.querySelectorAll('input[type="radio"]').forEach(el => el.checked = false);
+	document.querySelectorAll('textarea').forEach(el => el.value = "");
+	setDisplays()
     // Manage displayed content according to "henviser" selection
-	var idSelection = document.getElementById("selectHenviser");
+	// var selectHenviser = document.getElementById("selectHenviser");
 	var classAlmen = document.getElementsByClassName("almen");
 	var classSpecial = document.getElementsByClassName("special");
 	var classShared = document.getElementsByClassName("shared");
 	var classShowIf = document.getElementsByClassName("showIf");
-    
+
 	var dispStateAlmen = "none";
 	var dispStateSpecial = "none";
 	var dispStateShared = "none";
-    var i;
-    for (i = 0; i < classShowIf.length; i++) {
-    	classShowIf[i].style.display= "none";
-    }
-
-	if(idSelection.value == "intet"){
+	var i;
+	for (i = 0; i < classShowIf.length; i++) {
+		classShowIf[i].style.display= "none";
+	}
+	if (selectHenviser.value == "intet"){
 		dispStateAlmen = "none";
 		dispStateSpecial = "none";
 		dispStateShared = "none";
 	}
-
-	if(idSelection.value == "almen"){
+	if (selectHenviser.value == "almen"){
 		dispStateAlmen = "block";
 		dispStateSpecial = "none";
 		dispStateShared = "block";
 	}
-
-	if(idSelection.value == "special"){	
+	if (selectHenviser.value == "special"){	
 		dispStateAlmen = "none";
 		dispStateSpecial = "block";
 		dispStateShared = "block";
 	}
 
-    var i;
-    for (i = 0; i < classAlmen.length; i++) {
-    	classAlmen[i].style.display = dispStateAlmen;
-    } 
-    var i;
-    for (i = 0; i < classSpecial.length; i++) {
-    	classSpecial[i].style.display = dispStateSpecial;
-    } 
-    var i;
-    for (i = 0; i < classShared.length; i++) {
-    	classShared[i].style.display = dispStateShared;
-    } 	
+	var i;
+	for (i = 0; i < classAlmen.length; i++) {
+		classAlmen[i].style.display = dispStateAlmen;
+	} 
+	var i;
+	for (i = 0; i < classSpecial.length; i++) {
+		classSpecial[i].style.display = dispStateSpecial;
+	} 
+	var i;
+	for (i = 0; i < classShared.length; i++) {
+		classShared[i].style.display = dispStateShared;
+	} 	
 }
 
 function setDisplays(){
+
+
 	// checkboxes
 	if (checkboxKarpal.checked == true){
 		rowKarpal.style.display = "block";
@@ -81,7 +80,6 @@ function setDisplays(){
 	else {
 		rowAndet.style.display = "none";
 	}
-
 	// radios 
 	if (radioAk1.checked == true){
 		textareaAk.style.display = "block";
@@ -130,7 +128,7 @@ function setDisplays(){
 		dispStateMepMeg = "block";
 		dispStateMep = "block";
 		dispStateMeg = "block";	
-    }
+	}
 	if(checkboxMep.checked == true && checkboxMeg.checked == false){
 		dispStateMepMeg = "block";
 		dispStateMep = "block";
@@ -147,73 +145,127 @@ function setDisplays(){
 		dispStateMeg = "none";			
 	}
 
-    var i;
-    for (i = 0; i < classMepMeg.length; i++) {
-    	classMepMeg[i].style.display= dispStateMepMeg;
-    } 
-    var i;
-    for (i = 0; i < classMep.length; i++) {
-    	classMep[i].style.display= dispStateMep;
-    } 
-    var i;
-    for (i = 0; i < classMeg.length; i++) {
-    	classMeg[i].style.display= dispStateMeg;
-    } 	
+	var i;
+	for (i = 0; i < classMepMeg.length; i++) {
+		classMepMeg[i].style.display= dispStateMepMeg;
+	} 
+	var i;
+	for (i = 0; i < classMep.length; i++) {
+		classMep[i].style.display= dispStateMep;
+	} 
+	var i;
+	for (i = 0; i < classMeg.length; i++) {
+		classMeg[i].style.display= dispStateMeg;
+	} 	
 }
 
 function returnIfChecked(e, text) {
-	if ( document.getElementById(e).checked == true ) {
-		return "<br/>"+text;
+	if (document.getElementById(e).checked == true ) {
+		return text;
 	} else {
 		return '';
 	} 
 }
 
 function returnIfSet(e, text) {
-	if ( e.value.length > 0 ) {
-		return "<br/>"+text;
+	if (document.getElementById(e).value.length > 0 ) {
+		return text;
 	} else {
 		return '';
 	}
 }
 
-
-
 function  generateResult() {
-	var lb = "<br/>";
+	var lineBreak = "<br/>";
 	var tab = "&nbsp &nbsp";
-	var idKarpal = document.getElementById("selectKarpal")
-	var textKarpal = idKarpal.options[idKarpal.selectedIndex].text;	
+	var textKarpal = selectKarpal.options[selectKarpal.selectedIndex].text;
+	
+	// prints
+	var printDiagnostik = returnIfSet("textareaDiagnostisk", textareaDiagnostisk.value);
+	var printKarpal = returnIfChecked("checkboxKarpal", lineBreak+"Karpaltunnel, "+textKarpal);
+	var printEeg = returnIfChecked("checkboxEeg", lineBreak+"EEG"); 
+	var printEmg = returnIfChecked("checkboxEmg", lineBreak+"EMG"+lineBreak
+		+"Er pt. i AK-behandling? "+returnIfChecked("radioAk1", "Ja, "+textareaAk.value)+returnIfChecked("radioAk2", "Nej"));
+	
+	var printVep = returnIfChecked("checkboxVep", lineBreak+"VEP");
+	var printSep = returnIfChecked("checkboxSep", lineBreak+"SEP");
+	var printMep = returnIfChecked("checkboxMep", lineBreak+"MEP"+lineBreak
+		+"Har pt. interkranielle metalclips?: "+returnIfChecked("radioInter1", "Ja")+returnIfChecked("radioInter2", "Nej")+lineBreak
+		+"Har pt. metal i øjne el. ører?: "+returnIfChecked("radioØjne1", "Ja")+returnIfChecked("radioØjne2", "Nej")+lineBreak
+		+"Lider pt. af epilepsi?: "+returnIfChecked("radioEpilepsi1", "Ja")+returnIfChecked("radioEpilepsi2", "Nej")+lineBreak
+		+"Er pt. gravid?: "+returnIfChecked("radioGravid1", "Ja")+returnIfChecked("radioGravid2", "Nej"));
+	
+	var printMeg = returnIfChecked("checkboxMeg", lineBreak+"MEG"+lineBreak
+		+"Har pt. interkranielle metalclips?: "+returnIfChecked("radioInter1", "Ja")+returnIfChecked("radioInter2", "Nej")+lineBreak
+		+"Har pt. metal i øjne el. ører?: "+returnIfChecked("radioØjne1", "Ja")+returnIfChecked("radioØjne2", "Nej")+lineBreak
+		+"Har pt. bøjle el. metal i munden?: "+returnIfChecked("radioBøjle1", "Ja")+returnIfChecked("radioBøjle2", "Nej")+lineBreak
+		+"Har pt. metal i kroppen?: "+returnIfChecked("radioKrop1", "Ja")+returnIfChecked("radioKrop2", "Nej")+lineBreak
+		+"Er pt. gravid?: "+returnIfChecked("radioGravid1", "Ja")+returnIfChecked("radioGravid2", "Nej"));
+	
+	var printSøvn = returnIfChecked("checkboxSøvn", lineBreak+"Søvn");
+	var printØjen = returnIfChecked("checkboxØjen", lineBreak+"Øjen"
+		+returnIfChecked("checkboxVepØjen",lineBreak+"VEP ")
+		+returnIfChecked("checkboxErgØjen",lineBreak+"ERG ")
+		+returnIfChecked("checkboxSepØjen",lineBreak+"SEP "));
 
-	var textOut = 
-	"Diagnostisk spørgsmål, der ønskes besvaret:"
-	+returnIfSet(textareaDiagnostisk, textareaDiagnostisk.value)
-	+lb
-	+lb+"Ønskede undersøgelser:"
-	+tab+returnIfChecked("checkboxKarpal", "Karpaltunnel, "+"["+textKarpal+"]")+returnIfChecked("checkboxEeg", "EEG")+returnIfChecked("checkboxEmg", "EMG")+returnIfChecked("checkboxVep", "VEP")+returnIfChecked("checkboxSep", "SEP")+returnIfChecked("checkboxMep", "MEP")+returnIfChecked("checkboxMeg", "MEG")+returnIfChecked("checkboxSøvn", "søvn")+returnIfChecked("checkboxØjen", "Øjen")+returnIfChecked("checkboxIom", "IOM")+returnIfChecked("checkboxAndet", "Andet")
-	+lb
-	+lb+"Særlige behov? "+returnIfChecked("radioSærlige1", "Ja, "+textareaSærlige.value)+returnIfChecked("radioSærlige2", "Nej")
-	+lb
-	+lb+"Anamnese og objektive fund:"
-	+returnIfSet(textareaAnamnese, textareaAnamnese.value)
-	+lb+lb
+	var printIom = returnIfChecked("checkboxIom", lineBreak+"IOM");
+	var printAndet = returnIfChecked("checkboxAndet", lineBreak+"Andet: "+textareaAndet.value);
+	var printSærligeAlmen = returnIfChecked("radioSærlige1", "Ja, "+textareaSærlige.value)+returnIfChecked("radioSærlige2", "Nej");
 
+	var printSærligeSpecial = 
+	"Akut/fast-track?: "+returnIfChecked("radioAkut1", "Ja, "+textareaAkut.value)+returnIfChecked("radioAkut2", "Nej")+lineBreak
+	+"Behov for tolk: "+returnIfChecked("radioTolk1", "Ja "+textareaTolk.value)+returnIfChecked("radioTolk2", "Nej")+lineBreak
+	+"Andre behov?: "+returnIfChecked("radioAndre1", "Ja "+textareaAndre.value)+returnIfChecked("radioAndre2", "Nej");
+	
+	var printAnamnese = returnIfSet("textareaAnamnese", lineBreak+textareaAnamnese.value);
+	var printOut = "";
 
+	if (selectHenviser.value == "almen"){
+		printOut = 
 
-	// +lb+"Akut/fast-track? "+returnIfChecked("radioAkut1", "Ja")+returnIfChecked("radioAkut2", "Nej")+returnIfChecked("radioAkut1", textareaAkut.value)
-	// +lb+"Behov for tolk? "+returnIfChecked("radioTolk1", "Ja")+returnIfChecked("radioTolk2", "Nej")+returnIfChecked("radioTolk1", textareaTolk.value)
-	// +lb+"Andre særlige forhold? "+returnIfChecked("radioAndre1", "Ja")+returnIfChecked("radioAndre2", "Nej")+returnIfChecked("radioAndre1", textareaAndre.value)      
-	;
+		lineBreak
 
-	document.getElementById("textResult").innerHTML = textOut;
-	selectElementContents(document.getElementById("textResult"));
+		+"Diagnostisk spørgsmål, der ønskes besvaret:"
+		+lineBreak+printDiagnostik+lineBreak+lineBreak
+
+		+"Ønskede undersøgelser:"
+		+printKarpal+printEeg+lineBreak+lineBreak
+
+		+"Særlige behov? "
+		+lineBreak+printSærligeAlmen+lineBreak+lineBreak
+
+		+"Anamnese og objektive fund:"
+		+printAnamnese+lineBreak
+
+		+lineBreak
+		;
+	}
+
+	if (selectHenviser.value == "special"){
+		printOut = 
+
+		lineBreak
+
+		+"Ønskede undersøgelser:"
+		+printKarpal+printEeg+printEmg+printVep+printSep+printMep+printMeg+printSøvn
+		+printØjen+printIom+printAndet+lineBreak+lineBreak	
+
+		+"Særlige behov? "
+		+lineBreak+printSærligeSpecial+lineBreak+lineBreak
+
+		+"Anamnese og objektive fund:"
+		+printAnamnese+lineBreak
+
+		+lineBreak
+		;
+	}
+
+	textResult.innerHTML = printOut;
+	selectElementContents(textResult);
 }
 
 function selectElementContents(e) {
-	var doc = document
-	, text = e
-	, range, selection
-	;    
+	var doc = document, text = e, range, selection;    
 	if (doc.body.createTextRange) { //ms
 		range = doc.body.createTextRange();
 		range.moveToElementText(text);
@@ -228,23 +280,21 @@ function selectElementContents(e) {
 	}
 }
 
-// function showIfChecked(box, e){
-// 	var checkBox = document.getElementById(box);
-// 	var element = document.getElementById(e);
-// 	if (checkBox.checked == true){
-// 		element.style.display = "block";
-// 	} 
-// 	else {
-// 		element.style.display = "none";
-// 	}
-// }
-
-// function displayElement(id) {
-// 	var element = document.getElementById(id);
-// 	element.style.display = "block";
-// }
-
-// function hideElement(id) {
-// 	var element = document.getElementById(id);
-// 	element.style.display = "none";
-// }
+// var checkboxKarpal = document.getElementById("checkboxKarpal");
+// var rowKarpal = document.getElementById("rowKarpal");
+// var checkboxEmg = document.getElementById("checkboxEmg");
+// var rowEmg = document.getElementById("rowEmg");
+// var checkboxØjen = document.getElementById("checkboxØjen");
+// var rowØjen = document.getElementById("rowØjen");
+// var checkboxAndet = document.getElementById("checkboxAndet");
+// var rowAndet = document.getElementById("rowAndet");
+// var radioAk1 = document.getElementById("radioAk1");
+// var textareaAk = document.getElementById("textareaAk");
+// var radioSærlige1 = document.getElementById("radioSærlige1");
+// var textareaSærlige = document.getElementById("textareaSærlige");
+// var radioAkut1 = document.getElementById("radioAkut1");
+// var textareaAkut = document.getElementById("textareaAkut");
+// var radioTolk1 = document.getElementById("radioTolk1");
+// var textareaTolk = document.getElementById("textareaTolk");
+// var radioAndre1 = document.getElementById("radioAndre1");
+// var textareaAndre = document.getElementById("textareaAndre");
